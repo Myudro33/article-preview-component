e31560e276c7b1e47c80368efbce7829d7842789
myudro33 <qanashvilinika7@gmail.com>
2024-08-22 19:08:04 +0400

    style: add tailwind preset styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {},
+    colors:{
+      "dark-grayish-blue":"hsl(217, 19%, 35%)",
+      "dark-blue":"hsl(214, 17%, 51%)",
+      "grayish-blue":"hsl(212, 23%, 69%)",
+      "light-grayish-blue":"hsl(210, 46%, 95%)"
+    },
+    screens:{
+      "xs":"375px",
+      "lg":"1440px"
+    }
   },
   plugins: [],
 }
