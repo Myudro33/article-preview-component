1b9d3038786d7998e50fab41745c5c25d5b363b4
myudro33 <qanashvilinika7@gmail.com>
2024-08-22 19:01:49 +0400

    chore: add assets

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-    <h1 class="text-red-500">test</h1>
+  <div>
+
+  </div>
 </template>
 
+<script setup>
+</script>
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
